e9e07972244b2dbe26ff2703a48555fa05f72595
kishan jogani <kishanjogani926@gmail.com>
2023-02-03 13:49:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:combo_1/homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Homescreen(),
+    ),
+  );
+}
